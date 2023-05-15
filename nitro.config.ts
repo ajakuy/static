bf54265d08b0e7770f8b83fd750c1049d7e7acf7
Nemure231 <neruhi5321@gmail.com>
2023-05-15 15:04:32 +0700

    feat: add redirect route

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@ import { defineNitroConfig } from 'nitropack/config'
 
 export default defineNitroConfig({
     routeRules: {
+        '/': { redirect: { to: 'https://ajakuy.com', statusCode: 301 } },
         '/api/v1/**': { static: true, cors: true, headers: { 'access-control-allow-methods': 'GET' } },
     }
 })
